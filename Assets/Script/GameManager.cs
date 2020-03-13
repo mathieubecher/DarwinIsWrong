699caaafd547908c4be52d6e924074867db6dfa5
Becher <mbelkhenchir@gmail.com>
2020-03-13 10:48:30 +0100

    Base of random gen of map

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,67 +6,12 @@ using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
-    public Cube defaultCube;
-    public List<Cube> CubeType;
-    public int WIDTH = 50;
-    public Cube[,] cubes;
-    
+    public Map map;
     public Camera main;
-// Start is called before the first frame update
-    void Awake()
+    // Start is called before the first frame update
+    private void Awake()
     {
-        cubes = new Cube[WIDTH,WIDTH];
-        for (int x = 0; x < WIDTH; ++x)
-        {
-            for (int z = 0; z < WIDTH; ++z)
-            {
-                Cube cube = Instantiate(defaultCube, new Vector3(x, 0, z), Quaternion.identity, transform);
-                cubes[x, z] = cube;
-                cube.SetPosition(x,z);
-            }
-        }
-
-        GenerateMap();
-        
         main = Camera.main;
-        main.transform.position = new Vector3(WIDTH/2,0,WIDTH/2);
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void GenerateMap()
-    {
-        List<Cube> toTreat = new List<Cube>();
-        List<Cube> treats = new List<Cube>();
-        for (int i = 0; i < 5; ++i)
-        {
-            Cube cube = cubes[(int) (Random.value * WIDTH), (int) (Random.value * WIDTH)];
-            cube = ChangeCube(cube);
-            toTreat.Add(cube);
-        }
-        List<Cube> next = new List<Cube>();
-        foreach (Cube treat in toTreat)
-        {
-            
-        }
-        toTreat = next;
-        next = new List<Cube>();
-    }
-
-    Cube ChangeCube(Cube cube)
-    {
-        Vector2 position = cube.position;
-        Destroy(cube.gameObject);
-        cubes[(int)position.x, (int)position.y] = null;
-        
-        Cube cubeSpawn = Instantiate(defaultCube, new Vector3(position.x, position.y), Quaternion.identity, transform);
-        cubes[(int)position.x, (int)position.y] = cube;
-        cube.SetPosition((int)position.x, (int)position.y);
-        return cubeSpawn;
+        main.transform.position = new Vector3(map.WIDTH/2,0,map.WIDTH/2);
     }
 }
--- Assets/Script/Map.cs
+++ Assets/Script/Map.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 public class Map : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Cube defaultCube;
+    public List<Cube> cubeType;
+    public int WIDTH = 50;
+    public Cube[,] cubes;
+    [HideInInspector]
+    public GameManager manager;
+    void Awake()
     {
+        manager = FindObjectOfType<GameManager>();
+        cubes = new Cube[WIDTH,WIDTH];
+        for (int x = 0; x < WIDTH; ++x)
+        {
+            for (int z = 0; z < WIDTH; ++z)
+            {
+                Cube cube = Instantiate(defaultCube, new Vector3(x, 0, z), Quaternion.identity, transform);
+                cubes[x, z] = cube;
+                cube.SetPosition(x,z);
+            }
+        }
+
+        GenerateMap();
+        
+        
         
     }
 
@@ -14,5 +34,38 @@ public class Map : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void GenerateMap()
+    {
+        List<Cube> toTreat = new List<Cube>();
+        List<Cube> treats = new List<Cube>();
+        for (int i = 0; i < 5; ++i)
+        {
+            Cube cube = cubes[(int) (Random.value * WIDTH), (int) (Random.value * WIDTH)];
+            int index = (int) Mathf.Floor(Random.value * cubeType.Count);
+            Debug.Log(index);
+            cube = ChangeCube(cube,cubeType[index]);
+            toTreat.Add(cube);
+        }
+        List<Cube> next = new List<Cube>();
+        foreach (Cube treat in toTreat)
+        {
+            
+        }
+        toTreat = next;
+        next = new List<Cube>();
+    }
+
+    Cube ChangeCube(Cube cube, Cube type)
+    {
+        Vector2 position = cube.position;
+        Destroy(cube.gameObject);
+        cubes[(int)position.x, (int)position.y] = null;
+        
+        Cube cubeSpawn = Instantiate(type, new Vector3(position.x,0, position.y), Quaternion.identity, transform);
+        cubes[(int)position.x, (int)position.y] = cubeSpawn;
+        cubeSpawn.SetPosition((int)position.x, (int)position.y);
+        return cubeSpawn;
     }
 }
